dd10d892fa8f0996075c6bef5f6ada27d12d2508
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-11 23:59:57 -0500

    Update map

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 function init(){
 	//alert('it works');
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8334669, lng: -87.6245049},
     zoom: 8
   });
 
+	/*var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/icon.ico'
+	});
+
+	var contentString = '<h1>Alpha Sigma Alpha</h1>;
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
+    */
 }
 
-window.addEventListener('load', init);
+google.maps.event.addDomListener(window, 'load', init);
