d46a445471d3f8751a988ad51f7b1f9c79fbb5cf
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-11 22:50:41 -0500

    Integrating map

---
+++ script.js
@@ -0,0 +1,10 @@
+function init(){
+	//alert('it works');
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+
+}
+
+window.addEventListener('load', init);
