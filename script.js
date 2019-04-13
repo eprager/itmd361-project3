a00bc90ae261b6a276432108748aed500463568a
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-13 15:33:07 -0500

    init to initMap

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function init(){ 
+function initMap(){ 
 
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 41.8334669, lng: -87.6245049},
 		zoom: 16});
 	}
 
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', initMap);
