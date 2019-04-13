7f09cc82f2d89f31aa69559b141b0a7ff57d81b8
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-13 15:31:41 -0500

    Change zoom level

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function init(){
 
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 41.8334669, lng: -87.6245049},
-		zoom: 20});
+		zoom: 16});
 	}
 
 google.maps.event.addDomListener(window, 'load', init);
