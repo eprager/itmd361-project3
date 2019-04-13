d8d1c7aba66394b67b4f1ea695985de7ed24570a
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-13 15:53:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function initMap(){ 
-
 	var emma = {lat: 41.8334669, lng: -87.6245049};
-	var map = new google.maps.Map(document.getElementById('map'), {center: emma, zoom: 13});
+	var map = new google.maps.Map(document.getElementById('map'), {center: emma, zoom: 14});
 	var marker = new google.maps.Marker({position: emma, map: map});
-
 }
 google.maps.event.addDomListener(window, 'load', initMap);
