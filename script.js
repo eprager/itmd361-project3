36258b723dfb7a83a8290dc2d139a205ced3575c
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-13 15:40:47 -0500

    Change bracket location to correct function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function initMap(){ 
 
 	var emma = {lat: -25.344, lng: 131.036};
-	var map = new google.maps.Map(document.getElementById('map'), {center: emma, zoom: 16});}
+	var map = new google.maps.Map(document.getElementById('map'), {center: emma, zoom: 13});
 	var marker = new google.maps.Marker({position: emma, map: map});
 
 }
