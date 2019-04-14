d8633bfb6bd43747e0b998b81ea67e42e5318409
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-13 21:32:29 -0500

    Change location

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function initMap() {
-	var emma = {lat: 41.8334669, lng: -87.6245049};
+	var emma = {lat: 41.868362, lng: -87.613138};
 	var map = new google.maps.Map(document.getElementById('map'), {center: emma, zoom: 14});
 
 	var infowindow = new google.maps.InfoWindow({
-		content: 'Alpha Sigma Alpha: 3340 S Michigan Ave, Chicago, IL 60616'
+		content: 'Shedd Aquarium: Lakefront Trail'
 	});
 
 	var marker = new google.maps.Marker({
 		position: emma,
 		map: map,
-		title: 'Alpha Sigma Alpha',
 		icon: 'images/location.png'
 	});
 	
