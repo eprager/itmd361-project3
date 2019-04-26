29371e7004b1b4e6187dcd69a7588c680d6b70fb
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-26 10:30:29 -0500

    Call init() when first image loads

--- photos.js
+++ photos.js
@@ -1,6 +1,11 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+function init() {
+  var slideIndex = 1;
+  showSlides(slideIndex);
+}
+
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -23,7 +28,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
