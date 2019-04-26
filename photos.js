b3201d568b7940a572471cd6cf389b3a45c554d8
Emma Prager <46731977+eprager@users.noreply.github.com>
2019-04-26 00:40:40 -0500

    Edit photos.js

--- photos.js
+++ photos.js
@@ -1,13 +1,3 @@
-/*function load(){
-	var slideIndex = 2;
-  showSlides(slideIndex);
-  alert("laod");
-}
-
-load();*/
-
-showSlides(1);
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -23,15 +13,15 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides polaroid fade");
+  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
